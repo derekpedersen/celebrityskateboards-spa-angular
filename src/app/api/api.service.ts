1c51f9d3c35e128061420bf1ddc749a91cb74cf4
Derek Pedersen <derekpedersen.com@gmail.com>
2018-11-11 19:57:38 -0800

    Upgraded to angular 7

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map'
 import { Jsonp } from '@angular/http/src/http';
