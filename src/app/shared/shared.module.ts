54b4019e1b9196bf88747ff8dc3e4426e00986b5
Derek Pedersen <derekpedersen.com@gmail.com>
2019-09-14 10:35:28 -0700

    Angular 8 here we come

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,7 +7,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { ApiService } from '../api/api.service';
 
-import { MatButtonModule, MatCardModule, MatIconModule, MatExpansionModule, MatMenuModule, MatToolbarModule, MatProgressSpinnerModule, MatListModule, MatGridListModule, MatSidenavModule, MatProgressBarModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
